52694e1a98a90ba82d14a2186ee851ec66401997
Agathe Oddon <agathe.oddon@gmail.com>
2012-05-26 17:58:37 +0200

    Revert "Revert "c'est bon j'ai réussi!""
    This reverts commit 129ea62ea8b6d2ae54c9beafb96cd7a821c44762.

--- Calculatrice/menu.cpp
+++ Calculatrice/menu.cpp
@@ -3,11 +3,8 @@
 
 void MainWindow::MenuComplexe()
 {
-    bool tmp=ui->actionMode_Complexe->isChecked();
-    if(tmp)
-        complexe="1";
-    else
-        complexe="0";
+    complexe=ui->actionMode_Complexe->isChecked();
+    MainWindow::MAJParam();
 }
 
 
@@ -17,6 +14,8 @@ void MainWindow::MenuEntier()
     ui->actionRationnel->setChecked(0);
     ui->actionEntier->setChecked(1);
     typeDeCste="entier";
+    MainWindow::MAJParam();
+
 }
 
 void MainWindow::MenuReel()
@@ -25,6 +24,8 @@ void MainWindow::MenuReel()
     ui->actionRationnel->setChecked(0);
     ui->actionReel->setChecked(1);
     typeDeCste="reel";
+    MainWindow::MAJParam();
+
 }
 
 void MainWindow::MenuRationnel()
@@ -33,15 +34,16 @@ void MainWindow::MenuRationnel()
     ui->actionEntier->setChecked(0);
     ui->actionRationnel->setChecked(1);
     typeDeCste="rationnel";
+    MainWindow::MAJParam();
+
 }
 
 void MainWindow::MenuClavier()
 {
-    bool tmp=ui->actionClavier->isChecked();
-    if(tmp)
-        clavier="1";
-    else
-        clavier="0";    // A voir comment on change l'interface en fonction du boolean clavier...
+    clavier=ui->actionClavier->isChecked();
+    MainWindow::MAJParam();
+
+    // A voir comment on change l'interface en fonction du boolean clavier...
 }
 
 void MainWindow::MenuDegres()
@@ -49,6 +51,8 @@ void MainWindow::MenuDegres()
     ui->actionRadians->setChecked(0);
     ui->actionDegres->setChecked(1);
     angle="degres";
+    MainWindow::MAJParam();
+
 }
 
 void MainWindow::MenuRadians()
@@ -56,4 +60,6 @@ void MainWindow::MenuRadians()
     ui->actionDegres->setChecked(0);
     ui->actionRadians->setChecked(1);
     angle="radians";
+    MainWindow::MAJParam();
+
 }
